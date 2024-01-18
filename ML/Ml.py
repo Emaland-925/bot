042b1afaa776c49351a43fa4de71dc1b54dacf04
Muzna <edithcharlesdarnay@gmail.com>
2024-01-18 15:09:00 +0300

    modifying Ml file

--- ML/Ml.py
+++ ML/Ml.py
@@ -4,7 +4,47 @@
 # In[12]:
 
 
-class WeatherData:
+#NEW
+class CityWeatherData:
+    def __init__(self, city):
+        self.city = city
+
+    def analyze_weather(self, ALLSKY, CLRSKY, temperature, pressure, moisture):
+        import pandas as pd
+        import numpy as np
+        from sklearn.preprocessing import StandardScaler
+        from sklearn.linear_model import RidgeCV
+        from sklearn.model_selection import train_test_split
+
+        file_path = f'ML/{self.city.capitalize()} 2020-2022.csv'
+        data = pd.read_csv(file_path)
+
+        X = data[['ALLSKY_SFC_SW_DWN', 'CLRSKY_SFC_SW_DWN', 'PS', 'T2M', 'RH2M']].values
+        y = data.iloc[:, -1].values
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
+        y_train_scaled = scaler.fit_transform(y_train.reshape(-1, 1)).flatten()
+        y_test_scaled = scaler.transform(y_test.reshape(-1, 1)).flatten()
+
+        reg = RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3], fit_intercept=False, cv=10).fit(X_train, y_train)
+
+        # Prepare new data for prediction
+        new_data = np.array([[float(ALLSKY), float(CLRSKY), float(pressure), float(temperature), float(moisture)]])
+
+        # Make predictions
+        predictions_linear = reg.predict(new_data)
+
+        return predictions_linear
+
+
+
+
+
+'''
     def jeddah(self):
         import pandas as pd
         
@@ -39,8 +79,9 @@ class WeatherData:
         y_pred2 = reg2.predict(X_test)
         
         
-        new_data = np.array([[weather_data.ALLSKY, weather_data.CLRSKY, weather_data.pressure, weather_data.temperature, weather_data.moisture]])
-        
+        #new_data = np.array([[weather_data.ALLSKY, weather_data.CLRSKY, weather_data.pressure, weather_data.temperature, weather_data.moisture]])
+        new_data = np.array([[float(self.ALLSKY), float(self.CLRSKY), float(self.pressure), float(self.temperature), float(self.moisture)]])
+
        
         predictions_linear = reg2.predict(new_data)
 
@@ -81,8 +122,9 @@ class WeatherData:
         y_pred2 = reg2.predict(X_test)
         
         
-        new_data = np.array([[weather_data.ALLSKY, weather_data.CLRSKY, weather_data.pressure, weather_data.temperature, weather_data.moisture]])
-        
+        #new_data = np.array([[weather_data.ALLSKY, weather_data.CLRSKY, weather_data.pressure, weather_data.temperature, weather_data.moisture]])
+        new_data = np.array([[float(self.ALLSKY), float(self.CLRSKY), float(self.pressure), float(self.temperature), float(self.moisture)]])
+
         
         predictions_linear = reg2.predict(new_data)
 
@@ -123,8 +165,9 @@ class WeatherData:
         y_pred2 = reg2.predict(X_test)
         
        
-        new_data = np.array([[weather_data.ALLSKY, weather_data.CLRSKY, weather_data.pressure, weather_data.temperature, weather_data.moisture]])
-        
+        #new_data = np.array([[weather_data.ALLSKY, weather_data.CLRSKY, weather_data.pressure, weather_data.temperature, weather_data.moisture]])
+        new_data = np.array([[float(self.ALLSKY), float(self.CLRSKY), float(self.pressure), float(self.temperature), float(self.moisture)]])
+
         
         predictions_linear = reg2.predict(new_data)
 
@@ -166,8 +209,9 @@ class WeatherData:
         y_pred2 = reg2.predict(X_test)
         
        
-        new_data = np.array([[weather_data.ALLSKY, weather_data.CLRSKY, weather_data.pressure, weather_data.temperature, weather_data.moisture]])
-        
+        #new_data = np.array([[weather_data.ALLSKY, weather_data.CLRSKY, weather_data.pressure, weather_data.temperature, weather_data.moisture]])
+        new_data = np.array([[float(self.ALLSKY), float(self.CLRSKY), float(self.pressure), float(self.temperature), float(self.moisture)]])
+
         
         predictions_linear = reg2.predict(new_data)
 
@@ -186,7 +230,7 @@ print("4 - Dammam")
 city_choice = int(input("Enter the number corresponding to the city: "))
 
 
-weather_data = WeatherData()
+weather_data = CityWeatherData()
 
 
 weather_data.ALLSKY = input("Enter ALLSKY value: ")
@@ -209,6 +253,6 @@ else:
 
 
 # In[ ]:
-
+'''
 
 
